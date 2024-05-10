a9d4b5543dc5eb3286ed410e2f404fb5d7bf1707
Kazi Ejajul <kejaj.777@gmail.com>
2024-05-10 19:25:21 +0200

    Sliding window - tow occurence

--- coding-patterns/sliding-window/maximum-length-substring-with-two-occurrence.py
+++ coding-patterns/sliding-window/maximum-length-substring-with-two-occurrence.py
@@ -28,11 +28,64 @@ Constraints:
 2 <= s.length <= 100
 s consists only of lowercase English letters.
 """
+from collections import defaultdict
 
 
-def maximumLengthSubstringWithTwoOccurrences(s: str) -> int:
-    print(s)
+def maximum_length_substring_default_dict(s: str) -> int:
+    n = len(s)
+    count = defaultdict(int)
+    start = 0
+    end = 0
+    max_length = 0
+
+    while end < n:
+        next_char = s[end]
+        count[next_char] += 1
+        print(count)
+
+        while count[next_char] == 3:
+            count[s[start]] -= 1
+            start += 1
+
+        max_length = max(max_length, end - start + 1)
+        end += 1
+
+    print(max_length)
 
 
-str = "bcbbbcba"
-maximumLengthSubstringWithTwoOccurrences(str)
+def maximum_length_substring(s: str) -> int:
+    n = len(s)
+    count = {}
+    start = 0
+    end = 0
+    max_length = 0
+
+    while end < n:
+
+        # print(end)
+        next_char = s[end]
+        # print("next", next_char)
+        count[next_char] = count.get(next_char, 0) + 1
+        # print(count)
+
+        #
+        while count[next_char] == 3:
+            # print("es")
+            # print(start)
+            # print()
+            count[s[start]] -= 1
+            start += 1
+            # print("ad", count)
+        #
+
+        max_length = max(max_length, end - start + 1)
+        # print(next_char)
+        end += 1
+        # print("after end", end)
+        # print("mx", max_length)
+        # print()
+    print("max", max_length)
+
+
+input_string = "bcbbbcba"
+maximum_length_substring_default_dict(input_string)
