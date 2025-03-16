bcfc66b429b2c04459c318ba800dac91846fccd7
Kazi Ejajul <kejaj.777@gmail.com>
2025-03-16 02:05:21 +0100

    linkedlist and queue problmes

---
+++ linked-list/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,15 @@
+from typing import List
+from collections import Counter
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        res = len(students)
+        cnt = Counter(students)
+
+        for s in sandwiches:
+            if cnt[s] > 0:
+                res -= 1
+                cnt[s] -= 1
+            else:
+                break
+        
+        return res
